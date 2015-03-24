0aa56ec6885dbec7aca2f0883aac82f6cbb45d9b
Katie Harold <kharold@sjccs142.saintjoe.edu>
2015-03-24 15:12:16 -0400

    // homework file

--- golf.js
+++ golf.js
@@ -1,37 +1,42 @@
+// Katie Harold
+// CMP237 Ch. 9 Exercise Regexp Golf
+// Code from book
+
 // Fill in the regular expressions
 
-verify(/.../,
+verify(/ca[rt]/,
        ["my car", "bad cats"],
        ["camper", "high art"]);
 
-verify(/.../,
+verify(/pr?op/,
        ["pop culture", "mad props"],
        ["plop"]);
 
-verify(/.../,
+verify(/ferr(et|y|ari)/,
+		
        ["ferret", "ferry", "ferrari"],
        ["ferrum", "transfer A"]);
 
-verify(/.../,
+verify(/[a-z](ious)\b/,
        ["how delicious", "spacious room"],
        ["ruinous", "consciousness"]);
 
-verify(/.../,
+verify(/\s[.|,|;|:]/,
        ["bad punctuation ."],
        ["escape the dot"]);
 
-verify(/.../,
+verify(/\w[a-z]{6}/,
        ["hottentottententen"],
        ["no", "hotten totten tenten"]);
 
-verify(/.../,
+verify(/\b[a-df-z]+\b/,
        ["red platypus", "wobbling nest"],
        ["earth bed", "learning ape"]);
 
 
 function verify(regexp, yes, no) {
   // Ignore unfinished exercises
-  if (regexp.source == "...") return;
+  if (regexp.source == "golf.js") return;
   yes.forEach(function(s) {
     if (!regexp.test(s))
       console.log("Failure to match '" + s + "'");
